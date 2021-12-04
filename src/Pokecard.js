64a2d84ef63e81ece05aa78a8cba04ed7381d3d5
Sean M <smidgley792@gmail.com>
2021-12-04 08:51:56 -0800

    pokecard/pokedex

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import './Pokecard.css'
+
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component {
     render() {
+        let imgSrc = `${POKE_API}${this.props.id}.png`;
         return (
-            <div>Hello</div>
+            <div className="Pokecard">
+                <h1 className="Pokecard-title" >{this.props.name}</h1>
+                <img src={imgSrc} alt={this.props.id}/>
+                <div className="Pokecard-data">Type: {this.props.type}</div>
+                <div className="Pokecard-data">Lvl: {this.props.exp}</div>
+            </div>
         )
     }
 }
+
+export default Pokecard;
